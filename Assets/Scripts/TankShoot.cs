a82347fdf33f658f6602c61422a2ebd4b6b5afde
Anderson <andersongarciavarillas@gmail.com>
2022-06-20 19:57:51 -0500

    shoot with j, k and l

--- Assets/Scripts/TankShoot.cs
+++ Assets/Scripts/TankShoot.cs
@@ -8,12 +8,28 @@ public class TankShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject pj = Instantiate(pf_Projectile, this.transform.position, Quaternion.identity);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K)  || Input.GetKeyDown(KeyCode.L))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        GameObject pj = Instantiate(pf_Projectile, this.transform.position, Quaternion.identity);
+        StartCoroutine(DeleteProjectile(pj));
+    }
+
+    IEnumerator DeleteProjectile(GameObject projectile)
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(projectile);
     }
 }
