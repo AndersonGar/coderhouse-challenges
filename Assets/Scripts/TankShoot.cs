67d5727979cd0e30a6eaf18509c872ea68148d30
Anderson <andersongarciavarillas@gmail.com>
2022-06-25 14:17:01 -0500

    timer shoots

--- Assets/Scripts/TankShoot.cs
+++ Assets/Scripts/TankShoot.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class TankShoot : MonoBehaviour
 {
     public GameObject pf_Projectile;
+    GameObject projectile;
+    public float timer;
+    float time;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject pj = Instantiate(pf_Projectile, this.transform.position, Quaternion.identity);
+        time = timer;
+        Shoot();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Timer();
+    }
+
+    void Shoot()
+    {
+        projectile = Instantiate(pf_Projectile, this.transform.position, Quaternion.identity);
+    }
+
+    void Timer()
+    {
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            Destroy(projectile);
+            Shoot();
+            RestartTimer();
+        }
+    }
+
+    void RestartTimer()
+    {
+        time = timer;
     }
 }
